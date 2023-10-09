df29f48d248d3b2917faa08ffce68da4f02ecea1
DAW_M <aaroooooon060812@gmail.com>
2023-10-09 13:45:03 +0200

    V.2.0.0

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,6 +20,10 @@ import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
@@ -33,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(getString(R.string.happyText), getString(R.string.FromText))
+                    GreetingImage("Jetpack Compose Tutorial", "sadfgsafgsadf sadfg sad dsafgh sadfh sdf hsdgh sdghsgh sdg sadf sfg sdgfhs gsadf sd hsfh sfh sd sdfg sdf fdhj sf hsgh fhdfhd.","sadfgsafgsadf sadfg sad dsafgh sadfh sdf hsdgh sdghsgh sdg sadf sfg sdgfhs gsadf sd hsfh sfh sd sdfg sdf fdhj sf hsgh fhdfhd.")
                 }
             }
         }
@@ -41,49 +45,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, textTwo: String, modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         /*modifier = modifier.padding(8.dp)*/
         modifier = modifier
         ) {
         Text(
             text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
+            //fontSize = 100.sp,
+            fontSize = 25.sp,
+            //lineHeight = 116.sp,
             textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 36.sp,
+            fontSize = 20.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                //.align(alignment = Alignment.End)
+                .fillMaxWidth(),
+                textAlign = TextAlign.Justify
+
+        )
+        Text(
+            text = textTwo,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                //.align(alignment = Alignment.End)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Justify
 
         )
     }
 }
 
 @Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+fun GreetingImage(message: String, from: String, textTwo: String, modifier: Modifier = Modifier) {
     //val image = painterResource(R.drawable.androidparty)
     val image = painterResource(R.drawable.la_casa_del_dragon_logo_en_fuego_10590)
-    Box {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
         Image(
             painter = image,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            alpha = 0.5F
+            //alpha = 0.5F,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp) // Establece la altura deseada de 10px
         )
+        Spacer(modifier = Modifier.height(10.dp)) // Espacio vacío con la misma altura
         GreetingText(
             message = message,
             from = from,
+            textTwo = textTwo,
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .padding(8.dp)
         )
     }
-
 }
 
 @Preview(showBackground = true)
@@ -91,6 +115,6 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 fun BirthdayCardPreview() {
     MyApplicationTheme {
         //GreetingText("Happy Birthday Aarón!", "From Aarón")
-        GreetingImage("Happy Birthday Sam!", "From Emma")
+        GreetingImage("Happy Birthday Sam!", "From Emma","textTwo")
     }
 }
