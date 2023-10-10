a9737ebbc9d351146ac0b95b7d80bdd2a5a362c0
DAW_M <aaroooooon060812@gmail.com>
2023-10-10 09:39:35 +0200

    Ejercio 3 Revisarlo bien porque faltan cosas

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,12 +19,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
@@ -83,40 +86,113 @@ fun GreetingText(message: String, from: String, textTwo: String, modifier: Modif
 }
 
 @Composable
-fun GreetingImage(message: String,messageTwo:String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.la_casa_del_dragon_logo_en_fuego_10590)
+fun GreetingImage(message: String, messageTwo: String, modifier: Modifier = Modifier) {
 
     Box(
         modifier = modifier
             .fillMaxSize(),
-        contentAlignment = Alignment.Center
+        //contentAlignment = Alignment.Center
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(16.dp)
+                .background(color = Color.DarkGray)
         ) {
-            Image(
-                painter = image,
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
+            Row(
                 modifier = Modifier
-                    .size(200.dp) // Cambia el tamaño de la imagen según tus necesidades
-            )
-
-            Spacer(modifier = Modifier.height(16.dp)) // Espacio entre la imagen y el texto
-
-            Text(
-                text = message,
-                fontSize = 25.sp,
-                textAlign = TextAlign.Center
-            )
-
-            Spacer(modifier = Modifier.height(16.dp)) // Espacio entre la imagen y el texto
-
-            Text(
-                text = messageTwo,
-                fontSize = 25.sp,
-                textAlign = TextAlign.Center
-            )
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(8.dp)
+                        .background(color = Color.Blue) // Color de fondo para la primera columna
+                        .padding(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Text composable",
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                    Text(
+                        text = "Displays text and follows the recommended Material Design guidelines.",
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(8.dp)
+                        .background(color = Color.DarkGray) // Color de fondo para la segunda columna
+                        .padding(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Image composable",
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                    Text(
+                        text = "Creates a composable that lays out and draws a given Painter class object.",
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(8.dp)
+                        .background(color = Color.Red) // Color de fondo para la tercera columna
+                        .padding(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Row composable",
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                    Text(
+                        text = "A layout composable that places its children in a horizontal sequence.",
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(8.dp)
+                        .background(color = Color.Yellow) // Color de fondo para la cuarta columna
+                        .padding(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Column composable",
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "A layout composable that places its children in a vertical sequence.",
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
@@ -126,6 +202,6 @@ fun GreetingImage(message: String,messageTwo:String, modifier: Modifier = Modifi
 fun BirthdayCardPreview() {
     MyApplicationTheme {
         //GreetingText("Happy Birthday Aarón!", "From Aarón")
-        GreetingImage("La casa del dragón.","Temporada 2")
+        //GreetingImage("La casa del dragón.","Temporada 2")
     }
 }
